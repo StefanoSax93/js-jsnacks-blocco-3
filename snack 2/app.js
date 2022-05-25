6ccb1b6dca73ca6914b93cca0bdfd674990d997c
StefanoSax93 <101599907+StefanoSax93@users.noreply.github.com>
2022-05-25 15:47:18 +0200

    snack 2

--- snack 2/app.js
+++ snack 2/app.js
@@ -3,5 +3,23 @@ Fai inserire un numero, che chiameremo N, all’utente.
 Genera N array, ognuno formato da 10 numeri 
 casuali da 1 a 100. Ogni volta che ne crei uno, stampalo.*/
 
-const richiesta = prompt("inserisci il numero di liste che vuoi generare")
+//chiedo il numero desiderato
+const n = parseInt(prompt("Inserisci il numero di liste che vuoi generare"));
+//creo la lista che conterrà gli array
+const listaArray = [];
+//creo il ciclo while
+let i=0;
+let array; 
+
+while(i<n) {
+    array = [];
+    //faccio in modo che ogni array sia formato da 10 numeri casuali da 0 a 100
+    for(let x=0; x<10; x++) {
+        array[x]=Math.floor(Math.random() * 100)
+    }
+    listaArray[i]=array;
+    i++
+    console.log(array);
+}
+console.log(listaArray);
 
